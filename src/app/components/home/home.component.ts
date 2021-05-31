fd5ebef1c2dc4c857c2367ed7a149c48f80c7c1a
Mikael Stener <mikaelstener@gmail.com>
2021-05-31 16:07:19 +0200

    Adding debouncing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Video } from '../../models';
 import { HttpService } from '../../services/http.service';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,24 @@ import { HttpService } from '../../services/http.service';
 })
 export class HomeComponent implements OnInit {
   public videos: Array<Video> = [];
+  searchTextChanged: Subject<string> = new Subject<string>();
 
   constructor(
     private httpService: HttpService
   ) { }
 
   ngOnInit(): void { }
+
+  onChangeSearch(searchString: string) {
+    if (this.searchTextChanged.observers.length === 0) {
+      this.searchTextChanged
+        .pipe(debounceTime(500), distinctUntilChanged())
+        .subscribe(searchString => {
+          this.searchVideos(searchString);
+        });
+    }
+    this.searchTextChanged.next(searchString);
+  }
 
   searchVideos(search: string): void {
     if (!search) {
@@ -28,7 +42,7 @@ export class HomeComponent implements OnInit {
       });
   }
 
-  updateGrade(video: Video): void {
+  onChangeGrade(video: Video): void {
     this.httpService
       .updateGrade(video)
       .subscribe()
